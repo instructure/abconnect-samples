fdc0f862e3ee9bf32a077a9f376a8935b3a338df
George Menyhert <gmenyhert@certicasolutions.com>
2017-03-21 11:55:23 -0400

    Initial commit of new project - simplified example of how to integrate the embeddable Standards Browser widget

---
+++ standards-browser-min/ReadMe.md
@@ -0,0 +1,18 @@
+# Embeddable Standards Browser - Minimum
+## Installation
+Place all of the files/folders in one directory (download or clone).
+
+## Usage
+Open [StandardsBrowser.html](./StandardsBrowser.html) with a browser.  Enter your AB Connect partner ID and Key into the fields at the top of the page.  Tab/click away from the
+partner fields and the page will load.  Note that the capability in the dotted blue area is the embeddable standards browser widget.  See the init() function in [StandardsBrowser.js](./StandardsBrowser.js)
+for the code that launches the browser.  The rest of the code supplies the functionality of logging and displaying the raw standard details JSON.
+
+## Notes
+1. The embeddable widget API calls are not logged.  If you use the embeddable widget, you don't need to understand the details of the calls.  However, if you would like to seem them, check out
+the RelationshipBrowser.
+2. This sample requires a few third-party libraries to work. Most are referenced in their hosted locations in the HTML file but CryptoJS is not hosted online so it has been included here
+in the rollups and components folders. See the <script> statements at the top of the HTML file for details on the location of the project and licensing.
+
+## Known Issues
+1. Internet Explorer users will need to enable "cross domain" scripting.  To do this, go to Internet Options, Security, Custom Level, scroll down to "Miscellaneous" and set
+"Access data sources across domains" to *Enable*.  Alternatively another browser.
---
+++ standards-browser-min/StandardsBrowser.js
@@ -0,0 +1,241 @@
+const HOST = 'https://api.academicbenchmarks.com'
+const STANDARDS_URL = HOST + "/rest/v4/standards";
+const TOPICS_URL = HOST + "/rest/v4/topics";
+
+var gPartnerID = null;
+var gSignature = null;
+var gAuthExpires = null;
+var gTopicsLicensed = false;
+
+// Pull in a file that could be used to pre-populate fields.
+var imported = document.createElement('script');
+imported.src = '../fieldDefaults.js';
+document.head.appendChild(imported);
+
+jQuery.support.cors = true;
+//
+// authenticate - build the required credentials and load the page
+//
+function authenticate() {
+  gPartnerID = $('.partnerID').val().trim();
+  var partnerKey = $('.partnerKey').val().trim();
+  
+  if (gPartnerID.length === 0 || // we are still missing something
+    partnerKey.length === 0) {
+    return;
+  }
+  gAuthExpires = Math.floor(Date.now() / 1000) + 3600; // 1 hour lifespan (in seconds) note that "gAuthExpires" is in seconds, not milliseconds
+  //
+  // Build the signature
+  //
+  var message = '' + gAuthExpires + "\n";
+  //
+  // Build the token
+  //
+  var hash = CryptoJS.HmacSHA256(message, partnerKey);
+  gSignature = CryptoJS.enc.Base64.stringify(hash);
+  
+  checkTopicsLicenseLevel();
+}
+//
+// checkTopicsLicenseLevel - see if we can request concepts/topics
+//
+function checkTopicsLicenseLevel() {
+  //
+  // hit the topics endpoint - if you get a 401, you are not licensed for topics or concepts (or possibly don't have a valid ID/key - but either way, let's drop topics and concepts)
+  //
+  var topicURL = TOPICS_URL + '?limit=0' + authenticationParameters();
+  //
+  // request the data
+  //
+  $.ajax(
+    {
+    url: topicURL,
+    crossDomain: true,
+    dataType: 'json',
+    success: function(data,status) {
+        gTopicsLicensed = true; // note we can use topics
+        init();
+      },
+    error: function(req, status, error) {
+        if (req.status === 401) { // authorization error - let's figure out what kind
+          if (req.responseJSON.errors && 
+            req.responseJSON.errors[0].detail) {
+              
+            if (req.responseJSON.errors[0].detail === 'Signature is not authorized.') {
+              alert('Invalid partner ID or key.');
+            } else if (req.responseJSON.errors[0].detail === 'This account is not licensed to access Topics') {
+              // not going to do the Topic thing
+              init();
+            } else alert(error);
+          } else alert(error);
+        } else alert(error);
+      }
+    }
+  );
+}
+//
+// init - if we are here, it is all good - get started 
+//
+var gWidgetInit = false;
+function init() {
+  //
+  // Load the standards browser
+  //
+  if (gWidgetInit) {
+    $('.standard').standardsBrowser('destroy'); // this isn't strictly necessary, but we want to make sure it is cleared if someone changes the auth credentials and re-initializes
+  }
+  $('.standard').standardsBrowser({
+    singleSelectMode: true,
+    authCredentials: {
+      ID: gPartnerID,
+      signature: gSignature,
+      expires: gAuthExpires
+    },
+    onStandardSelect: function(event, GUID){
+      standardSelected(GUID);
+    },
+    onStandardDeselect: function(event, GUID){
+      noStandardSelected();
+    },
+    onError: function(event, message){
+      alert(message);
+    }
+  });
+  gWidgetInit = true;
+}
+//
+// authenticationParameters - retrieve the auth URL parameters
+//
+function authenticationParameters() {
+  var authDetails = '&partner.id=' + encodeURIComponent(gPartnerID);
+  if (gSignature) {
+    authDetails += '&auth.signature=' + encodeURIComponent(gSignature) + '&auth.expires=' + encodeURIComponent(gAuthExpires);
+  }
+  return authDetails;
+}
+//
+// standardSelected - make note of a change in the current standard. We retrieve the standard and display the details.
+//    currentStandard - the current selected GUID
+//
+//  Uses:
+//    gActiveStandard - last standard selected.  If it hasn't changed, we don't do anything.  If it changed,
+//      we display the standards data in the details. Otherwise do nothing.
+//
+var gActiveStandard = null;
+function standardSelected(currentStandard) {
+  //
+  // They selected a new node
+  //
+  if (gActiveStandard != currentStandard) {
+    //
+    // construct the URL to pull the details
+    //
+    var sourceUrl = STANDARDS_URL + '/' + currentStandard +
+      '?fields[standards]=statement,section,document,education_levels,disciplines,number,parent,utilizations';
+    if (gTopicsLicensed) sourceUrl += ',topics,concepts,key_ideas&include=topics,concepts'; // include the topics stuff if topics is licensed
+
+    logCall(sourceUrl); // dump URLs to the console to make it easy to see the calls when learning the API
+
+    sourceUrl += authenticationParameters(); // do the auth bit
+    //
+    // request the data
+    //
+    $.ajax(
+      {
+      url: sourceUrl,
+      crossDomain: true,
+      dataType: 'json',
+      success: function(data,status)
+        {
+        PopulateDetails(data);
+        
+        gActiveStandard = currentStandard; // remember where we are
+        },
+      error: function(req, status, error)
+        {
+        alert(error);
+        }
+      }
+    );
+  }
+}
+//
+// noStandardSelected - If the user deselects the standard, clear the details
+//
+//  Uses:
+//    gActiveStandard - last standard selected.  Clear it out to force a lookup next selection
+//
+function noStandardSelected() {
+  //
+  // Reset the selection state of everything
+  //
+  gActiveStandard = null;
+  $('.details').empty(); // clear the standard details
+}
+//
+// PopulateDetails - load the details section of the screen with the selected node details
+//
+//  Arguments:
+//    data - AJAX response
+//
+function PopulateDetails(data) {
+  //
+  // Let's start simple and just dump the JSON in pretty format
+  //
+  var jsonPretty = syntaxHighlightResponse(data); // prettify the JSON of the standard
+  
+  $('.details').html(jsonPretty); // update the details div with the content
+}
+//
+// toggleCommandLog - toggle the visibility of the API command log div area
+//
+function toggleCommandLog(URL) {
+  $('.commandArea').toggle();
+}
+//
+// logCall - dump the call URL to the command text area
+//
+//  Arguments:
+//    URL - the call
+//
+function logCall(URL) {
+  var textArea = $('.commandList');
+  textArea.val(textArea.val() + URL + "\n\n");
+  if(textArea.length)
+       textArea.scrollTop(textArea[0].scrollHeight - textArea.height());
+}
+//
+// syntaxHighlightResponse - pretify the response
+//
+//  Arguments:
+//    json - AJAX response
+//
+//  Response: HTML pretified JSON
+//
+function syntaxHighlightResponse(json) {
+  if (typeof json != 'string') {
+     json = JSON.stringify(json, null, 2);
+  }
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+    var cls = 'number';
+    var content = match;
+    if (/^"/.test(match)) {
+      if (/:$/.test(match)) {
+        cls = 'key';
+      } else {
+        if (/"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"/.test(match)) {
+          cls = 'guid';
+        } else {
+          cls = 'string';
+        }
+      }
+    } else if (/true|false/.test(match)) {
+      cls = 'boolean';
+    } else if (/null/.test(match)) {
+      cls = 'null';
+    }
+    return '<span class="' + cls + '">' + match + '</span>';
+  });
+}
